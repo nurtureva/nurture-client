9eb611ace7def9202966a9d8c24c65210a4959ca
Bob Nearents <bobnearents@gmail.com>
2024-05-24 00:05:12 -0700

    copy update

--- src/layouts/Dashboard/Dashboard.tsx
+++ src/layouts/Dashboard/Dashboard.tsx
@@ -42,7 +42,7 @@ export const Content = () => {
           including doulas for expert birth guidance, lactation support
           providers for nurturing breastfeeding/chestfeeding experiences, and
           perinatal mental health care professionals for emotional well-being.
-          <div></div>
+          <div><br/></div>
           Whether you're embarking on the adventure of parenthood for the first
           time or expanding your family, our aim is to ease the process of
           navigating this remarkable life chapter.
